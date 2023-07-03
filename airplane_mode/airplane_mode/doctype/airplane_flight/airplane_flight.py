91cbc5abaaf3424bdc21e8e8f4d533270e0b3310
Sandeep Kakde <sandeep@agkiya.com>
2023-07-03 17:43:36 +0530

    Completed day 2 Assignment

---
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2023, Sandeep Kakde and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AirplaneFlight(Document):
+	def before_submit(self):
+		self.status = "Completed"
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2023, Sandeep Kakde and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+import random
 
 
 class AirplaneTicket(Document):
-	pass
+	def validate(self):
+		unique_add_ons = []
+		add_ons = []
+		for add_on in self.add_ons:
+			if add_on.item not in unique_add_ons:
+				add_ons.append(add_on)
+				unique_add_ons.append(add_on.item)
+
+		self.add_ons = add_ons
+
+
+	def before_save(self):
+		random_integer = str(random.randint(0, 9))
+		random_alphabet = random.choice(['A', 'B', 'C', 'D', 'E'])
+		self.seat = random_integer + random_alphabet
+		total_amount = 0.0
+		for add_on in self.add_ons:
+			total_amount += add_on.amount
+
+		self.total_amount = total_amount + self.flight_price
+
+	def before_submit(self):
+		if self.status != "Boarded":
+			frappe.throw("Flight not boarded")
